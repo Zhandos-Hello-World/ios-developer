3a960b3dd285d058bb810af3d533ed5dd1f2c834
Baimurat Zhandos <76952603+Zhandos-Hello-World@users.noreply.github.com>
2022-05-06 19:00:39 +0600

    Created

---
+++ Homework-10.playground/Contents.swift
@@ -0,0 +1,89 @@
+import UIKit
+
+import SwiftUI
+
+struct Animal { let name: String }
+class Formatter {}
+
+class Document {
+    var name: String
+    var content: String
+    var url: URL?
+    var animal: Animal?
+
+    init(name: String, content: String, url: URL) {
+        self.name = name
+        self.content = content
+        self.url = url
+    }
+    
+    func send() -> Bool {
+        guard let url = url else {
+            return false
+        }
+        print(url)
+        // Отправка документа на сервер
+        return false
+    }
+    
+    func printAnimalName() {
+        print(animal?.name ?? "")
+    }
+}
+
+class PDF {
+    let document: Document
+    let service: Formatter
+
+    init(document: Document, service: Formatter) {
+        self.document = document
+        self.service = service
+    }
+
+    func create() -> Data {
+        // Тут мы можем создавать из документа Data
+        return Data()
+    }
+    
+    func parsingPDFtoPhone() {
+        // Тут парсим PDF для показа на разные устройства
+    }
+    
+    func parsingPDFtoComputer() {
+        // Тут парсим PDF для показа на разные устройства
+    }
+    
+    func parsingPDFtoPaper() {
+        // Тут парсим PDF для показа на разные устройства
+    }
+}
+
+class FileManager {
+    var service: Formatter?
+    
+    func sendPDF(_ pdf: PDF) {
+        service = pdf.service
+        // отправялем PDF
+    }
+    
+    func sendDocument(_ doc: Document) {
+        // отправялем Doc
+    }
+    
+    func savePDF(_ pdf: PDF) {
+        // сохраняем PDF
+    }
+    
+    func saveDocument(_ doc: Document) {
+        // сохраняем Doc
+    }
+}
+
+protocol Machine {
+    func convert(document: Document) -> PDF?
+    func convert(document: Document) -> Image?
+    func sendFax(document: Document)
+}
+
+class NewIphone {}
+class FaxMachine {}
